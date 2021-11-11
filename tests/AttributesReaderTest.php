920d0e358b459003f01bf0d702b4aa52a1270f88
mmasiukevich <contacts@desperado.dev>
2021-11-11 10:58:43 +0100

    code style fix

--- tests/AttributesReaderTest.php
+++ tests/AttributesReaderTest.php
@@ -1,4 +1,6 @@
-<?php /** @noinspection PhpUnhandledExceptionInspection */
+<?php
+
+/** @noinspection PhpUnhandledExceptionInspection */
 
 /**
  * PHP Service Bus annotations reader component.
@@ -8,7 +10,7 @@
  * @license https://opensource.org/licenses/MIT
  */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace ServiceBus\AnnotationsReader\Tests;
 
